6c55373a3991d1ab47a143d34a1bb1357672d0fe
alptekin.karabacak <alptekin.karabacak@ulakhaberlesme.com.tr>
2022-11-24 15:51:34 +0300

    First commit

---
+++ Classes/GameLoop.cpp
@@ -0,0 +1,30 @@
+#include "GameLoop.h"
+
+#include "DataContainer.h"
+#include "Tap.h"
+
+namespace Game
+{
+    void GameLoop::ReceiveData(Data::DataContainer& data)
+    {
+        // Triggers some related code to get and set the game data
+    }
+    
+    void GameLoop::Run()
+    {
+        // Runs some related and required code.
+    }
+        
+    void GameLoop::OnTap(const Interaction::Tap& tap)
+    {
+        
+    }
+    void GameLoop::OnScore(const int score, const GameTypes::eGameSide)
+    {
+        
+    }
+    void GameLoop::OnMatchResult(const GameTypes::eGameSide)
+    {
+        
+    }
+}
