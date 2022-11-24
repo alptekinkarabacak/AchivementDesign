78ce22ff2dcf332c538160f3eec7a8639e0184c5
alptekin.karabacak <alptekin.karabacak@ulakhaberlesme.com.tr>
2022-11-24 15:56:55 +0300

    First commit

---
+++ Classes/AchievementDefinition.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace Achievement
+{
+    // A data class holding the definition of an achievement.
+    class AchievementDefinition
+    {
+        public:
+        std::string ImagePath = "";
+        std::string Title = "";
+        std::string Text = "";
+        // RewardTypes and RewardAmounts map one to one, which means
+        // RewardTypes[0] has its reward amount at RewardAmount[0],
+        // RewardTypes[1] has its reward amount at RewardAmount[1],
+        // and so on.
+        std::vector<std::string> RewardTypes;
+        std::vector<std::string> RewardAmount;
+    };
+}
